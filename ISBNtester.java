eb3aca5362e6f8ccf2cf46c50e809f2609d6b47b
Crazydill8 <90478552+Crazydill8@users.noreply.github.com>
2021-11-15 13:58:59 -0500

    Update ISBNtester.java

--- ISBNtester.java
+++ ISBNtester.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.io.*;
 
 public class ISBNtester {
 	
 	 static boolean isValidISBN(String isbn)
 	    {
+		 	
 		 	String[] numbers = isbn.split("");
 		 	if(numbers[9].equals("X") || numbers[9].equals("x"))
 		 		numbers[9] = "10";
@@ -17,11 +19,11 @@ public class ISBNtester {
 			 	}
 		 	}catch(NumberFormatException e) {}
 		 	
-		 	// length must be 10
 	        int n = isbn.length();
 	        
 	        int sum; //initialized weighted sum
 	        
+	        // length must be 10
 	        if (n != 10)
 	            return false;
 	        
@@ -34,19 +36,8 @@ public class ISBNtester {
 	        	
 	        	sum = (Numbers[0]*10)+(Numbers[1]*9)+(Numbers[2]*8)+(Numbers[3]*7)+(Numbers[4]*6)+(Numbers[5]*5)+(Numbers[6]*4)+(Numbers[7]*3)+(Numbers[8]*2)+(Numbers[9]*1);  
 	      	    System.out.println(sum);
-	        
-	        /*
-	        	//prevents incorrect input
-		 	 	else if (lastChar != 'X' && (lastChar < '0' ||
-		 	 			lastChar > '9'))
-		 		 return false;
-		 		
-		 	 		/*else if(lastChar != 'X' && (lastChar > '0'
-		 	 			&& lastChar < '9')) { //creates sum if last digit is not X 
-		 	 				sum = (firstChar*10)+(secondChar*9)+(thirdChar*8)+(fourthChar*7)+(fifthChar*6)+(sixthChar*5)+(seventhChar*4)+(eighthChar*3)+(ninthChar*2)+(lastChar*1);
-		 	 		}
-		 	 	*/
-	        
+	       
+	      	    
 	 
 	        // Return true if weighted sum
 	        // of digits is divisible by 11.
@@ -61,21 +52,27 @@ public class ISBNtester {
 	 	
 	    public static void main(String[] args)
 	    {
-	    	Scanner input = new Scanner(System.in);
+	    	/*
+	    	 * This section needs to loop through each line of text file,
+	    	 * and inserts isbns of each line into two different files 
+	    	 * based on correct or incorrect
+	    	 */
+	    	
+	    	Scanner input = new Scanner("testFile.txt");
 	    	
 	    	
 	    		//takes an ISBN number and uses the 
 	    		//isValidISBN function to test it
 	    		System.out.println("Enter an ISBN number");
-	    		String isbn = input.nextLine();
 	    	
 	    		
-	    		if (isValidISBN(isbn))
+	    		if (isValidISBN())
 	    			System.out.println("Valid");
+	    		//put isbn into valid file here
 	    		else
 	    			System.out.println("Invalid");
-	        
-	    			//Checks if code should be ran again
+	    		//put isbn into invalid file here
+	    	
 	    	    input.close();
 	    	    System.exit(1);
 	    			
